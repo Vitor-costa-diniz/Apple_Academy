6e693b114aee1c625b5746970d152a90160b8fe4
Tiago Vargas <tiago.ce10@outlook.com>
2023-05-11 19:05:48 -0300

    Save and read tasks to/from Desktop

--- projects/ToDo/ToDo/TodoLIst.swift
+++ projects/ToDo/ToDo/TodoLIst.swift
@@ -10,10 +10,17 @@ import Foundation
 public class TodoList {
     
     public static func listAllTasks(tasks: [Tasks]) {
-       for task in tasks {
-           print(task.asString())
-       }
-   }
+        for task in tasks {
+            print(task.asString())
+        }
+    }
     
+    public static func convertTasksToString(tasks: [Tasks]) -> String {
+        var text = ""
+        for task in tasks {
+            text += task.asString()
+        }
+        return text
+    }
 }
 
--- projects/ToDo/ToDo/main.swift
+++ projects/ToDo/ToDo/main.swift
@@ -16,3 +16,20 @@ repeat {
     rawUserInput = readLine()
     
 } while rawUserInput?.lowercased() != "y"
+
+func getTasksPathFromDesktop() -> URL {
+    let documents = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)
+    let tasksFilePath: URL = documents[0]  // This is an assumption!
+    return tasksFilePath
+}
+
+func saveTasksToDesktop(tasks: [Tasks], fileName: String) -> Void {
+    let filePath: URL = getTasksPathFromDesktop().appending(path: fileName)
+    let allTasksAsString = TodoList.convertTasksToString(tasks: tasks)
+    let textToWrite = allTasksAsString
+    do {
+        try textToWrite.write(to: filePath, atomically: true, encoding: .utf8)
+    } catch {
+        print(error.localizedDescription)
+    }
+}
