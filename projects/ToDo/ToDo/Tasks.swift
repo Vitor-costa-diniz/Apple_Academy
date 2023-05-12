853a57eabeae2dcf8be2cf7a9bcdd9a20f87ac03
Tiago Vargas <tiago.ce10@outlook.com>
2023-05-12 18:56:56 -0300

    Parse tasks from text

--- projects/ToDo/ToDo/Tasks.swift
+++ projects/ToDo/ToDo/Tasks.swift
@@ -27,5 +27,27 @@ public class Tasks {
         }
     }
 
+    public static func parseMultipleTasks(from text: String) -> [Tasks] {
+        var tasks: [Tasks] = []
+        let texts = text.components(separatedBy: "\n")
+        for text in texts {
+            let task = parseTask(from: text)
+            tasks.append(task)
+        }
+        return tasks
+    }
+
+    private static func parseTask(from text: String) -> Tasks {
+        // `[ ] - Title 1\t\tDescription of task`
+        let indexOfFirstTab = text.firstIndex(of: "\t")!
+        let indexAfterCompletionStatusSeparator = text.firstIndex(of: "-")!
+        let title = String(text[indexAfterCompletionStatusSeparator..<indexOfFirstTab])
+
+        let indexOfLastTab = text.lastIndex(of: "\t")!
+        let description = String(text[text.index(after: indexOfLastTab)...])
+        let task = Tasks(title: title, description: description)
+
+        return task
+    }
 
 }
